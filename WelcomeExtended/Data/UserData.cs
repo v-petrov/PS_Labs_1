77e8779869e83ab541d6103869abe3a4a472d21c
v-petrov <vapetrov@tu-sofia.bg>
2024-03-10 10:53:35 +0200

    creating userData

---
+++ WelcomeExtended/Data/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+
+namespace WelcomeExtended.Data
+{
+    internal class UserData
+    {
+        private List<User> _users;
+        private int _nextId;
+        public UserData()
+        { 
+            _users = new List<User>();
+            _nextId = 0;
+        }
+    }
+}
