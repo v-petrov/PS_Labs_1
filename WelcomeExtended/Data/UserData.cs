9417eccf88617bf221f6e04a255e0d811d0a83dd
v-petrov <vapetrov@tu-sofia.bg>
2024-03-10 11:21:23 +0200

    adding functionality to userData class

--- WelcomeExtended/Data/UserData.cs
+++ WelcomeExtended/Data/UserData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Welcome.Model;
+using Welcome.Others;
 
 namespace WelcomeExtended.Data
 {
@@ -12,9 +13,64 @@ namespace WelcomeExtended.Data
         private List<User> _users;
         private int _nextId;
         public UserData()
-        { 
+        {
             _users = new List<User>();
             _nextId = 0;
         }
+        public void addUser(User user)
+        {
+            user.Id = _nextId++;
+            _users.Add(user);
+        }
+        public void deleteUser(User user)
+        {
+            _users.Remove(user);
+        }
+        public bool validateUser(string name, string password)
+        {
+            foreach (var user in _users)
+            {
+                if (user.Names == name && user.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool validateUserLambda(string name, string password)
+        {
+            return _users.Where(x => x.Names == name && x.Password == password)
+                          .FirstOrDefault() != null ? true : false;
+        }
+        public bool validateUserLinq(string name, string password)
+        {
+            var res = from user in _users
+                      where user.Names == name && user.Password == password
+                      select user.Id;
+            return res != null ? true : false;
+        }
+        public User getUser(string name, string password)
+        {
+            var res = from user in _users
+                      where user.Names == name && user.Password == password
+                      select user;
+            return res.FirstOrDefault();
+
+        }
+        public void assignUserRole(string name, UserRolesEnum userRole)
+        {
+            var res = from user in _users
+                      where user.Names == name
+                      select user;
+            if (res != null)
+            {
+                res.First().Role = userRole;
+            }
+            else 
+            {
+                Console.WriteLine("No such user found!");
+            }
+        }
+
     }
 }
