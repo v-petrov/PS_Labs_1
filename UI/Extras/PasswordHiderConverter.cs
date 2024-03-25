1600102d75214f06c1f9831d8a1e64cebadbd914
v-petrov <vapetrov@tu-sofia.bg>
2024-03-25 15:02:47 +0200

    adding hider password converter

---
+++ UI/Extras/PasswordHiderConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UI.Extras
+{
+    public class PasswordHiderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string password)
+            {
+                return new string('*', password.Length);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
