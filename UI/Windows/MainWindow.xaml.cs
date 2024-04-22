acda4b0592ca94fc58f10f79260e63db940cec18
v-petrov <vapetrov@tu-sofia.bg>
2024-04-22 13:16:52 +0300

    added details in UI

--- UI/Windows/MainWindow.xaml.cs
+++ UI/Windows/MainWindow.xaml.cs
@@ -20,5 +20,38 @@ namespace UI
         {
             InitializeComponent();
         }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void HideListVisibility_Click(object sender, RoutedEventArgs e)
+        {
+            studentsList.Visibility = studentsList.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void ChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = GetRandomColorBrush();
+        }
+
+        private void ChangeBorderColor_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush newColor = GetRandomColorBrush();
+            LeftBorder.Background = newColor;
+            RightBorder.Background = newColor;
+            TopBorder.Background = newColor;
+            BottomBorder.Background = newColor;
+        }
+
+        private SolidColorBrush GetRandomColorBrush()
+        {
+            Random rnd = new Random();
+            byte[] colorBytes = new byte[3];
+            rnd.NextBytes(colorBytes);
+            Color randomColor = Color.FromRgb(colorBytes[0], colorBytes[1], colorBytes[2]);
+            return new SolidColorBrush(randomColor);
+        }
     }
 }
